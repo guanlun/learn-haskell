66b13241b2af6af222a6259574ce901df22a06b3
guanlun <guanlunzhao@gmail.com>
2014-08-14 00:37:40 +0800

    added eval function

--- parser.hs
+++ parser.hs
@@ -3,25 +3,31 @@ data Operator = PLUS | MINUS | MULTIPLY | DIVIDE
 
 instance Show Operator where
     show PLUS = "+"
-    show MINUS = "+"
-    show MULTIPLY = "+"
-    show DIVIDE = "+"
+    show MINUS = "-"
+    show MULTIPLY = "*"
+    show DIVIDE = "/"
 
 data Node = 
-    OperatorNode {
+    IntNode {
+        value :: Int
+    }
+    | OperatorNode {
         operator :: Operator,
         leftChild :: Node, 
         rightChild :: Node
     }
-    | IntNode {
-        value :: Int
-    }
 
 instance Show Node where
+    show (IntNode v) = show v
     show (OperatorNode o l r) = "( " ++ show o ++ " " ++ 
         show l ++ " " ++ show r ++ " )"
 
-    show (IntNode v) = show v
+eval :: Node -> Int
+eval (IntNode i) = i
+eval (OperatorNode PLUS l r) = (eval l) + (eval r)
+eval (OperatorNode MINUS l r) = (eval l) - (eval r)
+eval (OperatorNode MULTIPLY l r) = (eval l) * (eval r)
+eval (OperatorNode DIVIDE l r) = (eval l) / (eval r)
 
 main = do
     f <- readFile "input.scm"
