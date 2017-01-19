11fd949d69101c82786e4751a0ac20e29bbda923
Guanlun Zhao <guanlunzhao@gmail.com>
2017-01-19 13:55:37 -0600

    Ray generation

--- learn.hs
+++ learn.hs
@@ -7,14 +7,39 @@ data Vec3 = Vec3 {
 add :: Vec3 -> Vec3 -> Vec3
 add v1 v2 = Vec3 ((x v1) + (x v2)) ((y v1) + (y v2)) ((z v1) + (z v2))
 
+minus :: Vec3 -> Vec3 -> Vec3
+minus v1 v2 = Vec3 ((x v1) - (x v2)) ((y v1) - (y v2)) ((z v1) - (z v2))
+
 data Shape =
     Sphere {
-        position :: Vec3,
+        center :: Vec3,
         radius :: Float
-    } | Plane {
+    }
+    | Plane {
         point :: Vec3,
         normal :: Vec3
     }
 
+data Ray = Ray {
+    startPos :: Vec3,
+    direction :: Vec3
+} deriving (Show)
+
+data Camera = Camera {
+    position :: Vec3,
+    front :: Vec3,
+    up :: Vec3
+}
+
+camera = Camera (Vec3 0 0 (-1)) (Vec3 0 0 1) (Vec3 0 1 0)
+
+viewPlaneCoords = [Vec3 x y 0 | x <- [1 .. 100], y <- [1 .. 80]]
+
+coordToRay :: Vec3 -> Vec3 -> Ray
+coordToRay startPos coord = Ray startPos (coord `minus` startPos)
+
+rayToColor :: Ray -> Int
+rayToColor _ = 0
+
 main = do
-    putStrLn $ show ((Vec3 1 2 3) `add` (Vec3 4 5 6))
+    putStrLn $ show (map (coordToRay (position camera)) viewPlaneCoords)
